ad57fa16f7cdca9ef8780d6972f2c48f3a164abd
chen2016 <74361579+2016cccc@users.noreply.github.com>
2023-03-22 11:50:38 +0800

    Update app.py
    第一个版本

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
-from flask import Flask, render_template, request, redirect, flash
-from datetime import datetime
-import os
+from flask import Flask, request, render_template
+import datetime
+import webbrowser
 
 app = Flask(__name__)
-app.secret_key = 'mysecret'
 
-messages_file = 'messages.txt'
+messages = []
 
-if not os.path.exists(messages_file):
-    with open(messages_file, 'w'):
-        pass
-
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    messages = []
-    with open(messages_file, 'r') as f:
-        for line in f:
-            message = line.strip().split(',')
-            messages.append(message)
-    messages.sort(key=lambda x: x[1], reverse=True)
-    return render_template('index.html.jinja2', messages=messages)
+    global messages
 
+    if request.method == "POST":
+        message = request.form["message"]
+        time = datetime.datetime.now()
+        messages.append((message, time))
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    content = request.form['content']
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with open(messages_file, 'a') as f:
-        f.write(f'{content},{timestamp}\n')
-    flash('Your message has been posted.')
-    return redirect('/')
+    return render_template("index.html.jinja2", messages=messages)
+
+if __name__ == "__main__":
+    webbrowser.open_new('http://localhost:5000')
+    app.run(debug=True)
