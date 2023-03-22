01813d2df9863b4ddd7ec17b73726185ed3f3575
chen2016 <74361579+2016cccc@users.noreply.github.com>
2023-03-22 11:51:58 +0800

    加入颜色功能和存储功能

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, request, render_template
 import datetime
 import webbrowser
+import os
 
 app = Flask(__name__)
 
 messages = []
+
+file_path = os.path.join(os.getcwd(), "messages.txt")
+
+def write_to_file(message_list):
+    with open(file_path, 'w') as f:
+        for message in message_list:
+            f.write(",".join(message) + "\n")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -12,11 +21,14 @@ def index():
 
     if request.method == "POST":
         message = request.form["message"]
+        color = request.form["colorpicker"]
         time = datetime.datetime.now()
-        messages.append((message, time))
+        messages.append((message, time.strftime("%Y-%m-%d %H:%M:%S"), color))
+        write_to_file(messages)
 
     return render_template("index.html.jinja2", messages=messages)
 
 if __name__ == "__main__":
     webbrowser.open_new('http://localhost:5000')
     app.run(debug=True)
+    
