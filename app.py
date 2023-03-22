ab097ff3e4f44b4256ebc8f3383cf51bfdf0034f
chen2016 <74361579+2016cccc@users.noreply.github.com>
2023-03-22 10:00:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request, redirect, flash
 from datetime import datetime
-import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = 'mysecret'
 
-conn = sqlite3.connect('messages.db', check_same_thread=False)
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS messages
-             (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, timestamp TEXT)''')
-conn.commit()
+messages_file = 'messages.txt'
+
+if not os.path.exists(messages_file):
+    with open(messages_file, 'w'):
+        pass
 
 
 @app.route('/')
 def index():
-    c.execute('SELECT * FROM messages ORDER BY timestamp DESC')
-    messages = c.fetchall()
+    messages = []
+    with open(messages_file, 'r') as f:
+        for line in f:
+            message = line.strip().split(',')
+            messages.append(message)
+    messages.sort(key=lambda x: x[1], reverse=True)
     return render_template('index.html.jinja2', messages=messages)
 
 
@@ -23,7 +27,7 @@ def index():
 def submit():
     content = request.form['content']
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    c.execute('INSERT INTO messages (content, timestamp) VALUES (?, ?)', (content, timestamp))
-    conn.commit()
+    with open(messages_file, 'a') as f:
+        f.write(f'{content},{timestamp}\n')
     flash('Your message has been posted.')
     return redirect('/')
